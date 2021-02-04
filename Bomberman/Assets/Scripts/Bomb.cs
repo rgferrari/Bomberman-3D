265f90208e2eaebfef7e5b2e6d0fec44f14d1bfc
ThiagoPavin <37277800+ThiagoPavin@users.noreply.github.com>
2021-02-04 20:28:49 -0300

    Added Unity project

---
+++ Bomberman/Assets/Scripts/Bomb.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    void Start ()
+    {
+        Invoke("Explode", 2f); //Call Explode in 3 seconds
+    }
+
+    void Explode ()
+    {
+        Destroy(gameObject, 0.3f); //Destroy the actual bomb in 0.3 seconds, after all coroutines have finished
+    }
+}
---
+++ Bomberman/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody Rig;
+
+    //Prefabs
+    public GameObject bombPrefab;
+
+    //Cached components
+    private Rigidbody rigidBody;
+    private Transform myTransform;
+    private Animator animator;
+
+    void Start() {
+        //Cache the attached components for better performance and less typing
+        rigidBody = GetComponent<Rigidbody> ();
+        myTransform = transform;
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector3 Position = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        
+        rigidBody.velocity = Position * speed;
+    }
+
+    void Update ()
+    {
+        DropBomb();
+    }
+
+    private void DropBomb() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (bombPrefab) 
+            {
+                Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x), 
+                            myTransform.position.y, Mathf.RoundToInt(myTransform.position.z)),
+                            bombPrefab.transform.rotation);  
+
+            }
+        }
+    }
+    
+}
