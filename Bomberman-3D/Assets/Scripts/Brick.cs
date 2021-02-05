c4b48ed41a25b2e26aba8a0b298c9b0c5117e133
rgferrari <renegferrari@gmail.com>
2021-02-05 00:39:37 -0300

    Added Brick gameobject

---
+++ Bomberman-3D/Assets/Scripts/Brick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy(){
+        Debug.Log("Dropped some item!");
+    }
+}
